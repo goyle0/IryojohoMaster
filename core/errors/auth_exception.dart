54ccb492587282b816a4aa9d0f1f4f9994bf7d59
goyle0 <f.syou0909@gmail.com>
2025-03-23 14:51:10 +0900

    不必要なファイルを削除・リファクタリング

--- core/errors/auth_exception.dart
+++
@@ -1,9 +0,0 @@
-class AuthException implements Exception {
-  final String message;
-
-  AuthException(this.message);
-
-  @override
-  String toString() => message;
-}
-
--- domain/repositories/question_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:iryojoho_master/domain/entities/question.dart';
-
-abstract class QuestionRepository {
-  Future<List<Question>> getQuestions({String? category, String? subCategory});
-  Future<Question> getQuestionById(String id);
-  Future<void> syncQuestions();
-}
-
